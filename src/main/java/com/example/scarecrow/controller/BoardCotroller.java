8be5fb533157da734d03315133d23ec0b503a888
JihwanCha11 <whgdmsdpseld@naver.com>
2023-12-13 11:10:50 +0900

    문의 게시판

--- src/main/java/com/example/scarecrow/controller/BoardCotroller.java
+++ src/main/java/com/example/scarecrow/controller/BoardCotroller.java
@@ -1,8 +1,20 @@
 package com.example.scarecrow.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.scarecrow.dto.Board;
+import com.example.scarecrow.dto.BoardForm;
+import com.example.scarecrow.service.BoardService;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/board")
@@ -10,4 +22,34 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class BoardCotroller {
 	
+	private final BoardService boardService;
+	//문의 목록
+	 @GetMapping("/list")
+	 public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+		 Page<Board> paging = this.boardService.getList(page);
+	     model.addAttribute("paging", paging);
+	     return "board/board_list";
+	 }
+	 //문의 상세
+	 @GetMapping(value = "/detail/{id}")
+	    public String detail(Model model, @PathVariable("id") Integer id) {
+		 	Board board = this.boardService.getBoard(id);
+	        model.addAttribute("board", board);
+	        return "board/board_detail";
+	    }
+	 //문의 등록
+	 @GetMapping("/create")
+	    public String boardCreate(BoardForm boardForm) {
+	        return "board/contact";
+	    }
+	 //문의 등록
+	 @PostMapping("/create")
+	    public String boardCreate(@Valid BoardForm boardForm, BindingResult bindingResult) {
+	        if (bindingResult.hasErrors()) {
+	            return "board/contact";
+	        }
+	     this.boardService.create(boardForm.getSubject(), boardForm.getContent());
+		 return "redirect:/board/list"; // 질문 저장후 질문목록으로 이동
+	    }
+
 }
--- src/main/java/com/example/scarecrow/repository/BoardRepository.java
+++ src/main/java/com/example/scarecrow/repository/BoardRepository.java
@@ -1,9 +1,14 @@
 package com.example.scarecrow.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.scarecrow.dto.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Integer>{
-	
+	//문의목록
+	Page<Board> findAll(Pageable pageable);
+	Page<Board> findAll(Specification<Board> spec, Pageable pageable);
 }
